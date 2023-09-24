6151b27f70d86fb2c6928bec37e02afadfaef940
03hoho03 <ghwjd321@naver.com>
2023-09-24 15:56:57 +0900

    design: outlet 부모 css 변경

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="flex justify-center items-center bg-black">
-      <div className="w-[360px] h-screen border bg-white flex flex-col justify-center items-center">
+    <div className="flex justify-center items-center bg-black overflow-y-scroll">
+      <div className="w-[360px] h-screen bg-white">
         <Outlet />
       </div>
     </div>
